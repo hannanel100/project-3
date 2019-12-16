a1d3fbd011c4199f3407d50a0c61654ae0b5e7db
hannanel gershinsky <hannanel100@gmail.com>
2019-12-16 15:47:55 +0200

    started with toturail

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 vacation tagging
 
 need to explain how to run this localy
+
+https://blog.logrocket.com/securing-a-react-app/
--- server/server.js
+++ server/server.js
@@ -1,18 +1,31 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const path = require('path');
+// const usersBl = require('./usersBL');
+// const vacationBl = require('./vacationBL');
+// const app = express();
+
+// const PORT = 3201;
+// const cors = require('cors');
+
+// app.use(cors());
+// app.use(express.static(path.join(__dirname, 'client')));
+// app.use(bodyParser.json());
+
+
+// app.listen(process.env.PORT || PORT, () =>
+//     console.log(`Example app listening on port ${process.env.PORT || PORT}!`),
+// );
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
-const usersBl = require('./usersBL');
-const vacationBl = require('./vacationBL');
 const app = express();
 
-const PORT = 3201;
-const cors = require('cors');
+const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'client')));
-app.use(bodyParser.json());
+app
+  .use(express.static(path.join(__dirname, '/client/build')))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-
-app.listen(process.env.PORT || PORT, () =>
-    console.log(`Example app listening on port ${process.env.PORT || PORT}!`),
-);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/vacations-client/build/index.html'));
+});
